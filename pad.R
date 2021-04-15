255682c5f6492a9dcbb80772a5eef12e058e7b63
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-04-15 17:49:32 +0200

    opzet structuur

--- pad.R
+++ pad.R
@@ -1,17 +1,18 @@
 
 
 # pad pc joost
-# pad_prj_schelde <- "G:/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
+pad_prj_schelde <- "G:/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
 # pad citrix joost
 # basis_pad <- "//Client/G$/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
 
 # pad pc Gunther
-pad_prj_schelde <- "G:/Mijn Drive/PRJ_SCHELDE/"
+# pad_prj_schelde <- "G:/Mijn Drive/PRJ_SCHELDE/"
 
-pad_moneos <- "VNSC/Rapportage_INBO/2020/"
+pad_moneos <- "VNSC/Rapportage_INBO/"
+pad_jaar <- "2021/"
 
 maak_pad <- function(hoofdstuk, onderdeel = NULL) {
-  pad <- paste0(pad_prj_schelde, pad_moneos, hoofdstuk, "/")
+  pad <- paste0(pad_prj_schelde, pad_moneos, pad_jaar, hoofdstuk, "/")
   if (!is.null(onderdeel))
     pad <- paste0(pad, onderdeel, "/")
   if (!dir.exists(pad))
