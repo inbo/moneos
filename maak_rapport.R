47b8296cef357f1ca33358bf49839774e694f8f2
ElsLommelen <els.lommelen@inbo.be>
2021-04-19 17:34:34 +0200

    codematige manier om hele rapport te bouwen, inclusief uitleg om knop in te stellen

---
+++ maak_rapport.R
@@ -0,0 +1,22 @@
+# Je kan het volledige rapport bouwen met de knop 'Build Book' op tab 'Build',
+# maar dan wordt het geopend en opgeslagen in de folder op github
+# -> output nog aanpassen in _bookdown.yml
+# Deze knop instellen (staat er normaal automatisch in dit package):
+# - kies in het menu Build > Configure Build Tools
+# - kies bij Project build tools 'Website'
+# - navigeer bij Site directory naar folder '150_geintegreerd_rapport'
+# - na klikken op OK verschijnt er een tab Build met knop 'Build Book'
+#   waar je vanaf nu op kan klikken om het hele rapport te maken
+
+# Alternatief kan je onderstaande code runnen, die het rapport wel op de gewenste plaats opslaat
+
+old_wd <- getwd()
+on.exit(setwd(old_wd))
+
+source("../pad.R")
+pad_output <- maak_pad("150_geintegreerd_rapport", "output")
+
+setwd("150_geintegreerd_rapport")
+bookdown::render_book("index.Rmd", output_dir = pad_output)
+
+setwd(old_wd)
