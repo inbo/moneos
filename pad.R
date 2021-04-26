6d67f403f0be1861f1207793daab796d0eaa6c6c
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-04-26 11:13:31 +0200

    invoegen aanpassingen Master

--- pad.R
+++ pad.R
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-
-
-# pad pc joost
-pad_prj_schelde <- "G:/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
-# pad citrix joost
-# basis_pad <- "//Client/G$/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
-# pad_prj_schelde <- "C:/Users/joost_vanoverbeke/Documents/"
-
-# pad pc Gunther
-# pad_prj_schelde <- "G:/Mijn Drive/PRJ_SCHELDE/"
-
-=======
 pad_prj_schelde <- Sys.getenv("pad_prj_schelde")
 if (pad_prj_schelde == "") {
   stop("Maak een bestand .Renviron aan met het pad waar PRJ_SCHELDE staat, zie de instructies voor deze repo.")
 }
->>>>>>> master
+
 pad_moneos <- "VNSC/Rapportage_INBO/"
 pad_jaar <- "2021/"
 
