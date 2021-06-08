b056c71598b8719881d326eeddf56268f4a8a905
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-06-08 11:38:24 +0200

    uitleg bij .Renviron

--- pad.R
+++ pad.R
@@ -2,6 +2,10 @@ pad_prj_schelde <- Sys.getenv("pad_prj_schelde")
 if (pad_prj_schelde == "") {
   stop("Maak een bestand .Renviron aan met het pad waar PRJ_SCHELDE staat, zie de instructies voor deze repo.")
 }
+# indien het bestand .Renviron nog niet bestaat, maak dit dan aan in de lokale moneos folder en voeg de volgende lijnen toe:
+# pad_prj_schelde="G:/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
+# basis_pad="//Client/G$/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
+
 
 pad_moneos <- "VNSC/Rapportage_INBO/"
 pad_jaar <- "2021/"
