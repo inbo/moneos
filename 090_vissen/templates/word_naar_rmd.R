c9a8a5ca12fecfa83e971a2da91c8ed6fd673faa
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-06-15 11:56:52 +0200

    testen word naar rmd

--- 090_vissen/templates/word_naar_rmd.R
+++ 090_vissen/templates/word_naar_rmd.R
@@ -5,7 +5,7 @@ convert_docx_to_rmd(from = "090_vissen/templates/template_vissen2.docx",
                     to = "template_vissen.Rmd",
                     dir = "090_vissen/templates")
 
-add_captions(from = "090_vissen/templates/template_vissen.Rmd",
-             to = "090_vissen/templates/template_vissen_captions.Rmd")
+add_captions(from = "090_vissen/templates/090_vissen.Rmd",
+             to = "090_vissen/templates/090_vissen_captions.Rmd")
 
 
