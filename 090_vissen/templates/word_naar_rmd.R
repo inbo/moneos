c1686f02f9f1dfee5967da705ec251be2d07003b
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-06-16 14:22:41 +0200

    templates word to rmd vervolg

--- 090_vissen/templates/word_naar_rmd.R
+++ 090_vissen/templates/word_naar_rmd.R
@@ -1,11 +1,9 @@
 
 library(protocolhelper)
 
-convert_docx_to_rmd(from = "090_vissen/templates/template_vissen2.docx",
-                    to = "template_vissen.Rmd",
-                    dir = "090_vissen/templates")
+convert_docx_to_rmd(from = "090_vissen/templates/broedvogels.docx")
 
-add_captions(from = "090_vissen/templates/template_vissen.Rmd",
-             to = "090_vissen/templates/template_vissen_captions.Rmd")
+add_captions(from = "090_vissen/templates/broedvogels.Rmd",
+             to = "090_vissen/templates/broedvogels_captions.Rmd")
 
 
