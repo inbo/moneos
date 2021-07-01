d2f6743bb0700d71ea02945754f23aa08a47d846
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-07-01 11:24:15 +0200

    ankerkuil

--- 090_vissen/templates/word_naar_rmd.R
+++ 090_vissen/templates/word_naar_rmd.R
@@ -1,9 +1,11 @@
 
 library(protocolhelper)
 
-convert_docx_to_rmd(from = "090_vissen/templates/broedvogels.docx")
+convert_docx_to_rmd(from = "090_vissen/templates/hogere planten.docx",
+                    to = "150_geintegreerd_rapport/hogere_planten_magweg.Rmd",
+                    dir_media = "../090_vissen/templates/figuren_geintegreerd-rapport")
 
-add_captions(from = "090_vissen/templates/broedvogels.Rmd",
-             to = "090_vissen/templates/broedvogels_captions.Rmd")
+add_captions(from = "150_geintegreerd_rapport/hogere_planten_magweg.Rmd",
+             to = "150_geintegreerd_rapport/050_hogere_planten.Rmd")
 
-
+unlink("150_geintegreerd_rapport/hogere_planten_magweg.Rmd")
