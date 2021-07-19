9c90d7376664fe5d0d467f1eba594c38b8600079
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-07-19 16:34:22 +0200

    rapportage ankerkuil

--- 090_vissen/templates/word_naar_rmd.R
+++ 090_vissen/templates/word_naar_rmd.R
@@ -1,11 +1,11 @@
 
 library(protocolhelper)
 
-convert_docx_to_rmd(from = "090_vissen/templates/hogere planten.docx",
-                    to = "150_geintegreerd_rapport/hogere_planten_magweg.Rmd",
-                    dir_media = "../090_vissen/templates/figuren_geintegreerd-rapport")
+convert_docx_to_rmd(from = "090_vissen/templates/template_vissen2.docx",
+                    to = "090_vissen/templates/template_vissen2_magweg.Rmd",
+                    dir_media = "figuren_geintegreerd-rapport")
 
-add_captions(from = "150_geintegreerd_rapport/hogere_planten_magweg.Rmd",
-             to = "150_geintegreerd_rapport/050_hogere_planten.Rmd")
+add_captions(from = "090_vissen/templates/template_vissen2_magweg.Rmd",
+             to = "090_vissen/templates/template_vissen2.Rmd")
 
-unlink("150_geintegreerd_rapport/hogere_planten_magweg.Rmd")
+unlink("090_vissen/templates/template_vissen2_magweg.Rmd")
