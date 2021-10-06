9e81eb74c89d3d195abecdae02f85c94601e926a
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2021-10-06 11:03:42 +0200

    hoofdstuk monitoring slik en schorrand

--- word_naar_rmd.R
+++ word_naar_rmd.R
@@ -1,11 +1,20 @@
 
 library(protocolhelper)
 
-convert_docx_to_rmd(from = "120_zoogdieren/120_Zoogdieren_20-07-2021.docx",
-                    to = "150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd",
-                    dir_media = "../120_zoogdieren/figuren_zoogdieren")
+# convert_docx_to_rmd(from = "120_zoogdieren/120_Zoogdieren_20-07-2021.docx",
+#                     to = "150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd",
+#                     dir_media = "../120_zoogdieren/figuren_zoogdieren")
+# 
+# add_captions(from = "150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd",
+#              to = "150_geintegreerd_rapport/120_Zoogdieren.Rmd")
+# 
+# unlink("150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd")
 
-add_captions(from = "150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd",
-             to = "150_geintegreerd_rapport/120_Zoogdieren.Rmd")
+convert_docx_to_rmd(from = "140_Monitoring_slik_schorrand/140_Monitoring_slik_schorrand.docx",
+                    to = "150_geintegreerd_rapport/140_Monitoring_slik_schorrand_magweg.Rmd",
+                    dir_media = "../140_Monitoring_slik_schorrand/figuren_slik_schorrand")
 
-unlink("150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd")
+add_captions(from = "150_geintegreerd_rapport/140_Monitoring_slik_schorrand_magweg.Rmd",
+             to = "150_geintegreerd_rapport/140_Monitoring_slik_schorrand.Rmd")
+
+unlink("150_geintegreerd_rapport/140_Monitoring_slik_schorrand_magweg.Rmd")
