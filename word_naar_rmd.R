aba55c2b030d1bbb1c4ebe2b799d12ea3c065c23
Joost Vanoverbeke <joost.vanoverbeke@inbo.be>
2022-10-03 16:56:01 +0200

    verplaatsen word_naar_rmd versie specifiek voor hoofdstuk om conflicten te vermijden

--- word_naar_rmd.R
+++ word_naar_rmd.R
@@ -1,14 +1,6 @@
 
 library(protocolhelper)
 
-source("../pad.R")
-
-hoofdstuk <- "040_vegetatiekaart"
-pad_data <- maak_pad(hoofdstuk, "data")
-pad_figuren <- maak_pad(hoofdstuk, "figuren")
-pad_tabellen <- maak_pad(hoofdstuk, "tabellen")
-
-
 # convert_docx_to_rmd(from = "120_zoogdieren/120_Zoogdieren_20-07-2021.docx",
 #                     to = "150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd",
 #                     dir_media = "../120_zoogdieren/figuren_zoogdieren")
@@ -18,13 +10,11 @@ pad_tabellen <- maak_pad(hoofdstuk, "tabellen")
 # 
 # unlink("150_geintegreerd_rapport/120_Zoogdieren_magweg.Rmd")
 
-convert_docx_to_rmd(from = "040_vegetatiekaart/040_vegetatiekaart.docx",
-                    to = "150_geintegreerd_rapport/040_vegetatiekaart_magweg.Rmd",
-                    dir_media = pad_figuren)
+convert_docx_to_rmd(from = "140_Monitoring_slik_schorrand/140_Monitoring_slik_schorrand.docx",
+                    to = "150_geintegreerd_rapport/140_Monitoring_slik_schorrand_magweg.Rmd",
+                    dir_media = "../140_Monitoring_slik_schorrand/figuren_slik_schorrand")
 
-add_captions(from = "150_geintegreerd_rapport/040_vegetatiekaart_magweg.Rmd",
-             to = "150_geintegreerd_rapport/040_vegetatiekaart.Rmd")
+add_captions(from = "150_geintegreerd_rapport/140_Monitoring_slik_schorrand_magweg.Rmd",
+             to = "150_geintegreerd_rapport/140_Monitoring_slik_schorrand.Rmd")
 
-unlink("150_geintegreerd_rapport/040_vegetatiekaart_magweg.Rmd")
-
-
+unlink("150_geintegreerd_rapport/140_Monitoring_slik_schorrand_magweg.Rmd")
