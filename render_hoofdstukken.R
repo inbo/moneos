6c42428fa2ee2bacbf3934b02e118465d93a9069
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2026-01-29 17:13:45 +0100

    correctie render hoofdstuk rename

--- render_hoofdstukken.R
+++ render_hoofdstukken.R
@@ -65,7 +65,7 @@ render_chapters <-
         
         # Check if the output exists, then rename it
         if (file.exists(default_output)) {
-          file.rename(default_output, final_name)
+          file.rename(default_output, final_name, overwrite = TRUE)
           message(paste("Successfully created:", final_name))
         } else {
           warning(paste("Could not find default output file:", default_output))
