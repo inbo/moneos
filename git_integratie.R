fc2a46c7cd0cd1f41dee56df51d6fa0dbc583825
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2026-01-29 17:11:02 +0100

    correctie integratie

--- git_integratie.R
+++ git_integratie.R
@@ -27,7 +27,7 @@ update_main <-
       system("git merge main")
       system(stringr::str_c("git push origin ", branch))
       system("git checkout main")
-      system("git merge 010_inleiding_en_overzicht")
+      system(stringr::str_c("git merge", branch))
       system("git push origin main")
       system(stringr::str_c("git checkout ", current))
     }
