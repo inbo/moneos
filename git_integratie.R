1988cce402ffa4d3bbf3bb571ace7a47700c9e34
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2026-03-23 14:28:43 +0100

    kleine aanpassing git integratie

--- git_integratie.R
+++ git_integratie.R
@@ -19,16 +19,18 @@ branches <-
 update_main <-
   function(branches) {
     current <- system("git branch --show-current", intern = TRUE)
-    for(branch in branches) {
-      # system("git checkout main")
-      system("git pull origin main")
-      system(stringr::str_c("git checkout ", branch))
-      system(stringr::str_c("git pull origin ", branch))
-      system("git merge main")
-      system(stringr::str_c("git push origin ", branch))
-      system("git checkout main")
-      system(stringr::str_c("git merge ", branch))
-      system("git push origin main")
+    for (i in c(1,2)) {
+      for(branch in branches) {
+        # system("git checkout main")
+        system("git pull origin main")
+        system(stringr::str_c("git checkout ", branch))
+        system(stringr::str_c("git pull origin ", branch))
+        system("git merge main")
+        system(stringr::str_c("git push origin ", branch))
+        system("git checkout main")
+        system(stringr::str_c("git merge ", branch))
+        system("git push origin main")
+      }
     }
     system(stringr::str_c("git checkout ", current))
   }
