a9b417a468352892c8233d260a0a91c4c212ec44
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2026-06-03 16:30:06 +0200

    git_merge_main

---
+++ git_merge_main.R
@@ -0,0 +1,38 @@
+
+# Note: You should be in the root directory of your RStudio project/Git repository
+
+branches <- 
+  c("010_inleiding_en_overzicht",
+    "030_ecotopen",
+    "040_vegetatiekaart",
+    "050_hogere_planten",
+    "060_sedimentkenmerken",
+    "070_macrozoobenthos",
+    "080_hyperbenthos",
+    "090_vissen",
+    "100_watervogels",
+    "110_broedvogels",
+    "120_zoogdieren",
+    "130_raaien",
+    "140_slik_schorrand")
+
+merge_main_to_branch <-
+  function(branches) {
+    current <- system("git branch --show-current", intern = TRUE)
+    for (i in c(1,2)) {
+      for(branch in branches) {
+        # system("git checkout main")
+        system("git pull origin main")
+        system(stringr::str_c("git checkout ", branch))
+        system(stringr::str_c("git pull origin ", branch))
+        system("git merge main")
+        system(stringr::str_c("git push origin ", branch))
+      }
+    }
+    system(stringr::str_c("git checkout ", current))
+  }
+
+update_main(branches)
+# update_main("010_inleiding_en_overzicht")
+
+
